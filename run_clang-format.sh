91aa47580bf08b02bc18e3bca8df284477a557ad
Gabriel Staples <ercaguy@gmail.com>
2021-02-08 00:36:58 -0800

    run_clang-format.sh: update regular expression to remove redundancy

--- run_clang-format.sh
+++ run_clang-format.sh
@@ -7,9 +7,9 @@
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
 
-# Find all files in THIS_DIR which end in .ino, .cpp, etc., as specified
+# Find all files in THIS_DIR which end in .ino, .cpp, .c, .h, etc., as specified
 # in the regular expression just below
-FILE_LIST="$(find "$THIS_DIR" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
+FILE_LIST="$(find "$THIS_DIR" | grep -E ".*\.(ino|cpp|c|h|hpp|hh)$")"
 
 # echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""  # for debugging
 
