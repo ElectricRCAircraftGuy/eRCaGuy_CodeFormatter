1a137dd96ad69d2cd0410b459a1e6f1cc73ea268
Gabriel Staples <ercaguy@gmail.com>
2022-03-27 23:58:18 -0700

    Update run_clang-format.sh to handle files with spaces in their names!
    Also prompt for confirmation to continue and modify the listed files.

--- run_clang-format.sh
+++ run_clang-format.sh
@@ -1,20 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # By Gabriel Staples
 # For documentation on this file, see my answer here:
-# https://stackoverflow.com/questions/28896909/how-to-call-clang-format-over-a-cpp-project-folder/65988393#65988393
+# [my answer] How to call clang-format over a cpp project folder?:
+# https://stackoverflow.com/a/65988393/4561887
 
-THIS_PATH="$(realpath "$0")"
-THIS_DIR="$(dirname "$THIS_PATH")"
+# See my ans: https://stackoverflow.com/a/60157372/4561887
+FULL_PATH_TO_SCRIPT="$(realpath "$0")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 
-# Find all files in THIS_DIR which end in .ino, .cpp, .c, .h, etc., as specified
-# in the regular expression just below
-FILE_LIST="$(find "$THIS_DIR" | grep -E ".*\.(ino|cpp|c|h|hpp|hh)$")"
+RETURN_CODE_SUCCESS=0
+RETURN_CODE_ERROR=1
 
-# echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""  # for debugging
+# Find all files in SCRIPT_DIRECTORY with one of these extensions
+FILE_LIST="$(find "$SCRIPT_DIRECTORY" | grep -E ".*\.(ino|cpp|c|h|hpp|hh)$")"
+# echo "\"$FILE_LIST\"" # debugging
+# split into an array; see my ans: https://stackoverflow.com/a/71575442/4561887
+# mapfile -t FILE_LIST_ARRAY <<< "$FILE_LIST"
+IFS=$'\n' read -r -d '' -a FILE_LIST_ARRAY <<< "$FILE_LIST"
+
+num_files="${#FILE_LIST_ARRAY[@]}"
+echo -e "$num_files files found to format:"
+if [ "$num_files" -eq 0 ]; then
+    echo "Nothing to do."
+    exit $RETURN_CODE_SUCCESS
+fi
+
+# print the list of all files
+for i in "${!FILE_LIST_ARRAY[@]}"; do
+    file="${FILE_LIST_ARRAY["$i"]}"
+    printf "  %2i: %s\n" $((i + 1)) "$file"
+done
+echo ""
+
+format_files="false"
+# See: https://stackoverflow.com/a/226724/4561887
+read -p "Do you wish to auto-format all of these files? [y|N] " user_response
+case "$user_response" in
+    [Yy]* ) format_files="true"
+esac
+
+if [ "$format_files" = "false" ]; then
+    echo "Aborting."
+    exit $RETURN_CODE_SUCCESS
+fi
 
 # Format each file.
-# - NB: do NOT put quotes around `$FILE_LIST` below or else the `clang-format` command will
-#   mistakenly see the entire blob of newline-separated file names as a SINGLE file name instead
-#   of as a new-line separated list of *many* file names!
-clang-format --verbose -i --style=file $FILE_LIST
+clang-format --verbose -i --style=file "${FILE_LIST_ARRAY[@]}"
